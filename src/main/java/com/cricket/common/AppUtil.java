91d6fa70849883b69489e388dd0722918a5965b5
Rajesh-kakawat <rajesh.kakawat97@gmail.com>
2020-09-19 17:47:54 +0530

    Added random string generation method

--- src/main/java/com/cricket/common/AppUtil.java
+++ src/main/java/com/cricket/common/AppUtil.java
@@ -1,11 +1,14 @@
 package com.cricket.common;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.mail.SimpleMailMessage;
 
 import com.cricket.model.User;
 
 public class AppUtil {
+
   public static SimpleMailMessage constructEmail(String subject, String body, User user, String from) {
     SimpleMailMessage email = new SimpleMailMessage();
     email.setSubject(subject);
@@ -14,6 +17,21 @@ public class AppUtil {
     email.setFrom(from);
     return email;
   }
+
+  public static String generateUniqueString(int length) {
+	  int leftLimit = 48; // numeral '0'
+	    int rightLimit = 122; // letter 'z'
+	    int targetStringLength = length;
+	    Random random = new Random();
+
+	    String generatedString = random.ints(leftLimit, rightLimit + 1)
+	      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+	      .limit(targetStringLength)
+	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+	      .toString();
+	    return generatedString;
+  }
+
 
   public static String createURL(HttpServletRequest request, String resourcePath) {
 
